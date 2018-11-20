4f009b192a5080165666e60459a6d47716d2e752
PatB93 <pat.b.irish@gmail.com>
2018-11-19 20:37:36 -0500

    Cleaning up code a bit before submission

--- app/src/main/java/com/example/pbode/brflickr/communcationlayer/DataTransferLayer.java
+++ app/src/main/java/com/example/pbode/brflickr/communcationlayer/DataTransferLayer.java
@@ -4,19 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DataTransferLayer {
-    
+
     // Source class, Transfer Object
     private static final Map<Object, TransferObject> data = new HashMap<>();
-    
+
     public static void addData(Object sourceClass, TransferObject transferObject) {
         data.put(sourceClass, transferObject);
     }
 
     public static TransferObject getData(Object sourceClass) {
-        return data.get(sourceClass);
-    }
-
-    public Map<Object, TransferObject> getMap() {
-        return data;
+        TransferObject transferObject = data.get(sourceClass);
+        data.remove(sourceClass);
+        return transferObject;
     }
 }
